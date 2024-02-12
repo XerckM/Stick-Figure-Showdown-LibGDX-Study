c1499aaa310213804698013772a9c6c987064fe1
Xerckiem <mercadoxerckiem@gmail.com>
2024-02-11 21:48:19 -0500

    initial commit

---
+++ core/src/com/xmdev/sfs/resources/Assets.java
@@ -0,0 +1,9 @@
+package com.xmdev.sfs.resources;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+public class Assets {
+    // Asset manager
+    public static final AssetManager manager = new AssetManager();
+
+}
