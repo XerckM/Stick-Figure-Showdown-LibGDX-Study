4c4534035b222812906f1dfbe7380075a5736250
Xerckiem <mercadoxerckiem@gmail.com>
2024-02-11 21:54:43 -0500

    added gameplay assets in asset manager

--- core/src/com/xmdev/sfs/resources/Assets.java
+++ core/src/com/xmdev/sfs/resources/Assets.java
@@ -6,4 +6,17 @@ public class Assets {
     // Asset manager
     public static final AssetManager manager = new AssetManager();
 
+    // Gameplay assets
+    public static final String BACKGROUND_TEXTURE = "textures/Background.png";
+    public static final String FRONT_ROPES_TEXTURE = "textures/FrontRopes.png";
+    public static final String IDLE_SPRITE_SHEET = "sprites/IdleSpriteSheet.png";
+    public static final String WALK_SPRITE_SHEET = "sprites/WalkSpriteSheet.png";
+    public static final String PUNCH_SPRITE_SHEET = "sprites/PunchSpriteSheet.png";
+    public static final String KICK_SPRITE_SHEET = "sprites/KickSpriteSheet.png";
+    public static final String HURT_SPRITE_SHEET = "sprites/HurtSpriteSheet.png";
+    public static final String BLOCK_SPRITE_SHEET = "sprites/BlockSpriteSheet.png";
+    public static final String WIN_SPRITE_SHEET = "sprites/WinSpriteSheet.png";
+    public static final String LOSE_SPRITE_SHEET = "sprites/LoseSpriteSheet.png";
+    public static final String GAMEPLAY_BUTTONS_ATLAS = "textures/GameplayButtons.atlas";
+    public static final String BLOOD_ATLAS = "textures/Blood.atlas";
 }
